80d5a42c9070e767601aabe5f67334a91c7d4996
lamalice20 <discord974a@gmail.com>
2024-09-12 11:00:45 +0200

    V0.1.4

--- websockethost/__init__.py
+++ websockethost/__init__.py
@@ -1,4 +1,4 @@
 from basicCGIserver import startCGIserver
 from basicserverhttp import start_server_http
-from client_socket import connect, chat
-from server_socket import bind_server, chat
+from socketServerClient.client import start_client_socket
+from socketServerClient.server import start_socket_server
