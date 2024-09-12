4bdb881c2a98d8ea1693a26b763fd621ecdf9798
lamalice20 <discord974a@gmail.com>
2024-09-12 16:12:37 +0200

    UPD(0.1.5)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as file:
 
 setup(
     name="websockethost",
-    version="0.1.4",
+    version="0.1.5",
     description="Web and python socket host (UPD)",
     package={"": ""},
     packages=find_packages(),
